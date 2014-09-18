d977d1d5cc2173075951a7234e51a4a9fad9d55e
Audun Skjervold <audun@skjervold.me>
2014-09-18 11:36:17 +0200

    Add defaults and failsafes
    Add default values and return points for values that are not supplied when called

--- src/tools.js
+++ src/tools.js
@@ -2,20 +2,32 @@ var input = require('yargs').argv;
 
 var generateValues = function (args) {
 	
-	var min = parseFloat(args.min),
-		max = parseFloat(args.max),
-		growth = parseFloat(args.growth);
+	var keys = Object.keys(args);
+
+	var min, max;
+
+	if (keys.indexOf('min') === -1 || keys.indexOf('max') === -1) {
+		return "Please enter both min and max values.";
+	}
+	else {
+		min = args.min;
+		max = args.max;
+	}
+
+	var growth = keys.indexOf('growth') != -1 ? args.growth : 0;
+	var numValues = keys.indexOf('numValues') != -1 ? args.numValues : 1;
+	var round = keys.indexOf('round') != -1 ? args.round : -1;
 
 	var val = 0,
 		output = '';
 
-	for (var i = 0; i < args.numValues; i++) {
+	for (var i = 0; i < numValues; i++) {
 		val = Math.random()*(max-min)+min;
-		if (args.round) {
+		if (round === 0) {
 			val = Math.floor(val);
 		}
-		else {
-			val = val.toFixed(1);
+		else if (round != -1) {
+			val = val.toFixed(round);
 		}
 
 		if (i < args.numValues-1) {
@@ -35,7 +47,7 @@ if (Object.keys(tools).indexOf(input.tool) != -1) {
 	console.log(tools[input.tool](input));
 }
 else {
-	console.log('No tool selected. Please select a tool with --tool=nameOfTool. \nAvailable tools are:');
+	console.log('No tool or invalid tool selected. Please select a tool with --tool=nameOfTool. \nAvailable tools are:');
 	var toolNames = Object.keys(tools);
 
 	toolNames.forEach(function (tool) {
